69d0629871a11baa553421bee54b0aabb6c73722
Olivier Wilkinson (reivilibre) <oliverw@matrix.org>
2021-10-14 11:46:10 +0100

    Add tests for the module

--- tests/__init__.py
+++ tests/__init__.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Dict, Any
+from typing import Any, Dict
 from unittest import mock
 
 from synapse.module_api import ModuleApi, UserID
@@ -20,14 +20,16 @@ from synapse.module_api import ModuleApi, UserID
 from synapse_user_restrictions.module import UserRestrictionsModule
 
 
-def create_module(config_dict: Dict[Any, Any], server_name: str = "example.com") -> UserRestrictionsModule:
+def create_module(
+    config_dict: Dict[Any, Any], server_name: str = "example.com"
+) -> UserRestrictionsModule:
     # OSTD is this useful to us?
     def get_qualified_user_id(localpart: str) -> str:
         return UserID(localpart, server_name).to_string()
 
     # Create a mock based on the ModuleApi spec, but override some mocked functions
-    # because some capabilities (interacting with the database, getting the current time,
-    # etc.) are needed for running the tests.
+    # because some capabilities (interacting with the database,
+    # getting the current time, etc.) are needed for running the tests.
     module_api = mock.Mock(spec=ModuleApi)
     module_api.get_qualified_user_id.side_effect = get_qualified_user_id
 
---
+++ tests/test_config.py
@@ -0,0 +1,81 @@
+import re
+import unittest
+
+from synapse.module_api.errors import ConfigError
+
+from synapse_user_restrictions import UserRestrictionsModule
+from synapse_user_restrictions.config import (
+    RegexMatchRule,
+    UserRestrictionsModuleConfig,
+)
+
+from tests import create_module
+
+
+class ExampleTest(unittest.IsolatedAsyncioTestCase):
+    def test_config_error_exceptions(self) -> None:
+        """
+        Check that configuration errors raise exceptions.
+        """
+        with self.assertRaisesRegex(ConfigError, ".*unterminated.*"):
+            create_module({"rules": [{"match": "@srtsrt[a-z", "allow": "invite"}]})
+
+        with self.assertRaisesRegex(
+            ConfigError, "'nonsense' is not a permission recognised"
+        ):
+            # The `nonsense` permission doesn't exist.
+            create_module({"rules": [{"match": "@bob:test", "allow": ["nonsense"]}]})
+
+        with self.assertRaises(ConfigError):
+            create_module({})
+
+        with self.assertRaises(ConfigError):
+            create_module({"roooolz": []})
+
+        with self.assertRaises(ConfigError):
+            create_module(
+                {
+                    "rules": True,
+                }
+            )
+
+        with self.assertRaises(ConfigError):
+            create_module(
+                {
+                    "rules": [False],
+                }
+            )
+
+        with self.assertRaises(ConfigError):
+            create_module(
+                {
+                    "rules": [{"maaartch": "bleh"}],
+                }
+            )
+
+    def test_config_correct(self) -> None:
+        self.assertEqual(
+            UserRestrictionsModule.parse_config(
+                {
+                    "rules": [
+                        {
+                            "match": "@unprivileged[0-9]+:.*",
+                            "allow": ["invite"],
+                            "deny": ["create_room"],
+                        }
+                    ],
+                    "default_allow": set(),
+                    "default_deny": {"invite", "create_room"},
+                }
+            ),
+            UserRestrictionsModuleConfig(
+                rules=[
+                    RegexMatchRule(
+                        re.compile("@unprivileged[0-9]+:.*"),
+                        allow={"invite"},
+                        deny={"create_room"},
+                    )
+                ],
+                default_deny={"invite", "create_room"},
+            ),
+        )
